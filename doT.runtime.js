2256242a7983537e3c411e870b665230f69097f8
mgutz <mario@mgutz.com>
2012-05-28 02:05:24 -0700

    separate runtime; made it easier to debug (use append:false, strip:false); move encode up to use {{{}}}

---
+++ doT.runtime.js
@@ -0,0 +1,11 @@
+;(function() {
+function encodeHTMLSource() {
+  var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;' },
+    matchHTML = /&(?!\\w+;)|<|>|"|'|\//g;
+  return function(code) {
+    return code ? code.toString().replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : code;
+  };
+}
+if (typeof window === 'undefined')  global.__dotEncodeHtml = encodeHTMLSource()
+else window.__dotEncodeHTML = encodeHTMLSource();
+})();
